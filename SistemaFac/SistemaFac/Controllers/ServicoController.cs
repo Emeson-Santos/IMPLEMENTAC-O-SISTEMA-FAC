20e22e72f2f7158f9dc0afb24816da6028f9cc07
paulo vitor <paulovitordossf@gmail.com>
2017-09-05 10:26:40 -0300

    ServicoController esta ok

---
+++ SistemaFac/SistemaFac/Controllers/ServicoController.cs
@@ -0,0 +1,116 @@
+﻿using System.Web.Mvc;
+using Model.Models;
+using Negocio.Business;
+
+namespace SistemasFAC.Controllers
+{
+    public class ServicoController : Controller
+    {
+        private GerenciadorServico gerenciador;
+
+        public ServicoController()
+        {
+            gerenciador = new GerenciadorServico();
+        }
+
+        public ActionResult Index()
+        {
+            return View(gerenciador.ObterTodos());
+        }
+
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(Servico servico)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    gerenciador.Adicionar(servico);
+                    return RedirectToAction("Index");
+                }
+            }
+            catch
+            {
+
+            }
+            return View();
+        }
+
+        // GET: Editora/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id.HasValue)
+            {
+                Servico servico = gerenciador.Obter(id);
+                if (servico != null)
+                {
+                    return View(servico);
+                }
+
+            }
+            return RedirectToAction("Index");
+        }
+
+        // POST: Editora/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, Servico servico)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    gerenciador.Editar(servico);
+                }
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+
+            }
+            return RedirectToAction("Index");
+        }
+
+        // GET: Editora/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id.HasValue)
+            {
+                Servico servico = gerenciador.Obter(id);
+                if (servico != null)
+                {
+                    return View();
+                }
+
+
+            }
+            return RedirectToAction("Index");
+        }
+
+        // POST: Editora/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, Servico servico)
+        {
+            try
+            {
+                gerenciador.Remover(servico);
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+
+            }
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ SistemaFac/SistemaFac/Controllers/TipoEventoController.cs
@@ -0,0 +1,130 @@
+﻿using System.Web.Mvc;
+using Model.Models;
+using Negocio.Business;
+using System.Collections.Generic;
+
+
+namespace SistemasFAC.Controllers
+{
+    public class TipoEventoController : Controller
+    {
+
+        private GerenciadorTipoEventos gerenciador;
+        private TipoEvento servico;
+
+        public TipoEventoController()
+        {
+            servico = new TipoEvento() { Id = 1 };
+            gerenciador = new GerenciadorTipoEventos();
+        }
+
+        public ActionResult Index()
+        {
+           
+            List<Servico> servicos = gerenciador.ObterTodos(servico.Id);
+            if (servicos==null || servicos.Count==1)
+            {
+                servicos = null;
+            }
+            return View(servicos);
+        }
+
+        public ActionResult Create()
+        {
+            ViewBag.ListaDescricao = new SelectList(gerenciador.ObterTodos(), "Descricao");
+            return View();
+        }
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(TipoEvento tipoevento)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    gerenciador.Adicionar(tipoevento);
+                    return RedirectToAction("Index");
+                }
+            }
+            catch
+            {
+
+            }
+            return View();
+        }
+
+        // GET: Editora/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id.HasValue)
+            {
+                TipoEvento tipoevento = gerenciador.Obter(id);
+                if (tipoevento != null)
+                {
+                    return View(tipoevento);
+                }
+
+            }
+            return RedirectToAction("Index");
+
+
+        }
+
+        // POST: Editora/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, TipoEvento tipoevento)
+        {
+            try
+            {
+
+                if (ModelState.IsValid)
+                {
+                    gerenciador.Editar(tipoevento);
+                }
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+
+            }
+            return RedirectToAction("Index");
+        }
+
+        // GET: Editora/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id.HasValue)
+            {
+                TipoEvento tipoevento = gerenciador.Obter(id);
+                if (tipoevento != null)
+                {
+                    return View();
+                }
+
+
+            }
+            return View();
+        }
+
+        // POST: Editora/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, TipoEvento tipoevento)
+        {
+            try
+            {
+                gerenciador.Remover(tipoevento);
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+    }
+}
